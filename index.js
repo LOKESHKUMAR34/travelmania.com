9a5e152064aca3f9bee11f7443f06ce95e340752
LOKESHKUMAR34 <106895536+LOKESHKUMAR34@users.noreply.github.com>
2022-06-18 10:30:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+    // 1.Ways to print in JavScript
+    // console.log("Hello world");
+    // alert("me")
+    // document.write("this is documaent write")
+    // 2. JavaScript console API
+    // console.log("Hello world, 6+4, Another");
+    // console.warn("this is warning")
+    // console.error("this is error")
+    // 3. Variables in JavaScript
+    var number1 = 31
+    var number2 = 32
+    console.log(number1+number2);
+
+    // 4. Data Types in JavaScript
+    // Numbers
+    var num1 = 46
+    var num2 = 40.56
+
+    // String
+    var str1 = "This is a string";
+    var str2 = "This is also string";
+
+    // Objects
+    var marks = {
+        ravi:50,
+        aman:60
+    }
+    console.log(marks)
+
+    //Boolens
+    var a = true;
+    var b = false;
+    console.log(a)
+
+    var und = undefined;
+    console.log(undefined)
+
+    var n = NaN
+    console.log(n);
+
+    /* At a very high level, there are two types of datatypes in javascript
+    1. Primitive data types; undefined,null,number,string,boolen,string
+    2. Refference data types; 
+
